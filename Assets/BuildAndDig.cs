56595941c42003efd16c33e9249c445e451aedf1
Joel Segersam <joel.segersam.0807@student.uu.se>
2024-11-25 17:47:33 +0100

    You can build with simple raycast

--- Assets/BuildAndDig.cs
+++ Assets/BuildAndDig.cs
@@ -4,10 +4,10 @@ using UnityEngine.InputSystem;
 public class BuildAndDig : MonoBehaviour
 {
     [SerializeField] TilePlacer tilePlacer;
+    [SerializeField] float rayDistance = 10f; // Maximum distance for the raytrace
 
-    private Vector2 aimDirection = Vector2.zero;
+    Vector2 aimDirection = Vector2.zero;
 
-    
     public void Build()
     {
         Debug.Log("Building");
@@ -23,10 +23,20 @@ public class BuildAndDig : MonoBehaviour
     public void OnAim(InputAction.CallbackContext context)
     {
         aimDirection = context.ReadValue<Vector2>().normalized;
-               
-        if (aimDirection == Vector2.zero)
+
+        if (aimDirection != Vector2.zero)
         {
-            aimDirection = Vector2.down;
+            Ray2D ray = new Ray2D(transform.position, aimDirection);
+
+            Debug.DrawRay(ray.origin, ray.direction * rayDistance, Color.red);
+
+            // 3 is Ground layer
+            RaycastHit2D hit = Physics2D.Raycast(ray.origin, ray.direction, rayDistance, LayerMask.GetMask("Ground"));
+
+            if (hit.collider != null)
+            {
+                tilePlacer.placeTile(hit.point);
+            }
         }
     }
 }
