2655cdde27f67248df647f8c20fa5513227fcb07
bomakarlsson <boman.carlsson@hotmail.com>
2024-11-25 21:16:59 +0100

    I dont know what this is

--- Assets/DestroyOnCollision.cs
+++ Assets/DestroyOnCollision.cs
@@ -2,18 +2,15 @@ using UnityEngine;
 
 public class DestroyOnCollision : MonoBehaviour
 {
-    [SerializeField] private GameObject particleEffectPrefab; // Assign your particle effect prefab in the inspector
+    [SerializeField] private GameObject particleEffectPrefab; 
 
     private void OnCollisionEnter2D(Collision2D collision)
-    {
-        // Check if the particleEffectPrefab is assigned
+    {       
         if (particleEffectPrefab != null)
-        {
-            // Instantiate the particle effect at the collision point
+        {           
             Instantiate(particleEffectPrefab, transform.position, Quaternion.identity);
         }
-
-        // Destroy the object
+                
         Destroy(gameObject);
     }
 }
