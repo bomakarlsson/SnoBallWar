ad038cad4f41c2ece3f0d436ddb385dadc5a6428
Joel Segersam <joel.segersam.0807@student.uu.se>
2024-11-28 18:24:03 +0100

    Added fill/remove indicator circles

--- Assets/BuildAndDig.cs
+++ Assets/BuildAndDig.cs
@@ -13,19 +13,28 @@ public class BuildAndDig : MonoBehaviour
 
 
     Vector2 aimDirection = Vector2.zero;
-    Vector2 lastAimDirection = Vector2.zero;
 
+    bool isInBuildMode = false;
     bool validBuild, validDig = false;
     Vector2 hitPosition = Vector2.zero;
 
     Coroutine buildCoroutine, digCoroutine;
 
-    DrawLineTo lineDrawer;
+    DrawLineTo aimDrawer;
+    SpriteRenderer BuildIndicator, DigIndicator;
 
     void Awake()
     {
-        lineDrawer = GetComponent<DrawLineTo>();
-        lineDrawer.enabled = false;
+        aimDrawer = GetComponent<DrawLineTo>();
+        aimDrawer.enabled = false;
+
+        BuildIndicator = transform.GetChild(1).GetComponent<SpriteRenderer>();
+        BuildIndicator.transform.localScale = Vector3.one * fillRadius;
+        BuildIndicator.enabled = false;
+
+        DigIndicator = transform.GetChild(0).GetComponent<SpriteRenderer>();
+        DigIndicator.transform.localScale = Vector3.one * fillRadius * digFillMultiplier;
+        DigIndicator.enabled = false;
     }
 
     public void Build(InputAction.CallbackContext context)
@@ -80,6 +89,7 @@ public class BuildAndDig : MonoBehaviour
                     tilePlacer.FillCircleWithTiles(hitPosition, fillRadius);
                 Debug.Log("Building");
             }
+            validBuild = false;
             yield return new WaitForSeconds(buildTime);
         }
     }
@@ -98,53 +108,77 @@ public class BuildAndDig : MonoBehaviour
                     tilePlacer.FillCircleWithTiles(hitPosition, fillRadius * digFillMultiplier, false);
                 Debug.Log("Digging");
             }
+            validDig = false;
             yield return new WaitForSeconds(digTime);
+        }
+    }
+
+    public void OnBuildMode(InputAction.CallbackContext context)
+    {
+        isInBuildMode = context.performed;
+
+        if (!isInBuildMode)
+        {
+            aimDrawer.enabled = false;
+            DigIndicator.enabled = false;
+            BuildIndicator.enabled = false;
         }
     }
 
     public void OnAim(InputAction.CallbackContext context)
     {
-        if (context.canceled)
+        if (!isInBuildMode)
         {
-            Debug.Log("DISABLELELLE");
-            lineDrawer.enabled = false;
             return;
         }
 
         aimDirection = context.ReadValue<Vector2>().normalized;
+
         if (aimDirection == Vector2.zero)
-            aimDirection = lastAimDirection;
+        {
+            aimDrawer.enabled = false;
+            DigIndicator.enabled = false;
+            BuildIndicator.enabled = false;
+            return;
+        }
 
         Ray2D ray = new Ray2D(transform.position, aimDirection);
+        
+        // Visual representation
         Debug.DrawRay(ray.origin, ray.direction * aimDistance, Color.red);
-        // VISUAL REPRESENTATION IN GAME
-        if (context.control.device.name != "Mouse")
-        {
-            lineDrawer.target = ray.origin + ray.direction * aimDistance;
-            lineDrawer.enabled = true;
-        }
+        aimDrawer.target = (Vector2)transform.position + aimDirection * aimDistance;
+        aimDrawer.enabled = true;
 
         RaycastHit2D groundHit = Physics2D.Raycast(ray.origin, ray.direction, aimDistance, LayerMask.GetMask("Ground"));
-        if (groundHit.collider != null)
-        {
-            hitPosition = groundHit.point;
-            validDig = true;
-
-            // OBS checks for all colliders in player layer
-            // (should maybe be square cast)
-            RaycastHit2D playerHit = Physics2D.CircleCast(groundHit.point, fillRadius, Vector2.zero, 0f, LayerMask.GetMask("Player"));
-            if (playerHit.collider == null)
-                validBuild = true;
-            else
-                validBuild = false;
-        }
-        else
+        if (groundHit.collider == null)
         {
             validDig = false;
             validBuild = false;
+            DigIndicator.enabled = false;
+            BuildIndicator.enabled = false;
+            return;
         }
 
-        if (context.performed)
-            lastAimDirection = aimDirection;
+        hitPosition = groundHit.point;
+        validDig = true;
+
+        // Visual representation
+        DigIndicator.transform.position = groundHit.point;
+        DigIndicator.enabled = true;
+
+        // OBS checks for all colliders in player layer
+        // (should maybe be square cast)
+        RaycastHit2D playerHit = Physics2D.CircleCast(groundHit.point, fillRadius, Vector2.zero, 0f, LayerMask.GetMask("Player"));
+        if (playerHit.collider == null)
+        {
+            validBuild = true;
+            BuildIndicator.transform.position = groundHit.point;
+            BuildIndicator.enabled = true;
+        }
+        else
+        {
+            validBuild = false;
+            BuildIndicator.enabled = false;
+        }
     }
 }
